683bbfc30e9ab96df4c9e7711a6c5e3fdfa5a1c6
Phelipe Alves Fagundes <81323315+howjack@users.noreply.github.com>
2021-08-30 18:58:59 -0300

    Update index.jsx

--- web-app/src/pages/Consulta/index.jsx
+++ web-app/src/pages/Consulta/index.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Header from "../../components/Header";
 import Modal from "../../components/Modal";
+import SearchBar from "../../components/SearchBar";
 import StudentList from "../../components/StudentsList";
 
 class Consulta extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { edit: false };
+		this.state = {
+			edit: false,
+		};
+
 		this.editBtn = this.editBtn.bind(this);
 	}
 
@@ -15,13 +19,13 @@ class Consulta extends React.Component {
 		this.setState((state) => {
 			return { edit: !state.edit };
 		});
-		console.log(this.state.edit);
 	}
 
 	render() {
 		return (
 			<>
 				<Header title="Lista de alunos" />
+				<SearchBar />
 				<StudentList editBtn={this.editBtn} />
 				{this.state.edit && <Modal text="editado" />}
 			</>
