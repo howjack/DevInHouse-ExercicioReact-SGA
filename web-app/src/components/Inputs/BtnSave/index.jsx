9701ba7c03de8cd030ea23b36bb21fab6c2ed03a
Phelipe Alves Fagundes <81323315+howjack@users.noreply.github.com>
2021-09-01 18:01:06 -0300

    Button component

---
+++ web-app/src/components/Inputs/BtnSave/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class BtnSave extends React.Component {
+	render() {
+		return (
+			<button type="submit" disabled>
+				Salvar
+			</button>
+		);
+	}
+}
+
+export default BtnSave;
