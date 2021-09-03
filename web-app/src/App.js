1184e50f2050ca300a3ba1666608f9434d82999e
Phelipe Alves Fagundes <81323315+howjack@users.noreply.github.com>
2021-09-03 20:08:22 -0300

    Mudança condicional de page

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -4,17 +4,32 @@ import Consulta from './pages/Consulta';
 import Cadastro from './pages/Registration';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
 
-  
+    this.state = { page: true, student: "" }
+
+    this.studentEdit = "";
+    this.handleChange = this.handleChange.bind(this)
+    this.onEdit = this.onEdit.bind(this)
+  }
+
+  handleChange() {
+    this.setState(() => ({ page: !this.state.page }))
+  }
+  onEdit(event) {
+    this.setState(() => ({ student: event.props.nome }),
+      () => {
+        this.setState(() => ({ page: !this.state.page }))
+      }
+    )
+  }
 
   render() {
-
-    let page = false;
-
     return (
       <>
-        {page && <Consulta />}
-        {!page && <Cadastro />}
+        {this.state.page && <Consulta onTransition={this.handleChange} onEdit={this.onEdit} />}
+        {!this.state.page && <Cadastro onTransition={this.handleChange} studentEdit={this.state.student} />}
       </>)
   }
 }
