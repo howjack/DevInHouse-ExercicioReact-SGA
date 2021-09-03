f604056d197cc8ef32313e011c5d82737e0c04af
Phelipe Alves Fagundes <81323315+howjack@users.noreply.github.com>
2021-09-03 20:10:15 -0300

    ícones de mais e seta para mudar Page

--- web-app/src/components/Header/index.jsx
+++ web-app/src/components/Header/index.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React from "react";
+import ArrowImg from "../../Assets/arrow.png";
+import AddImg from "../../Assets/plus.png";
 
-class Header extends React.Component{
-
-    render(){
-        return (
-        <header className="header">
-            <h1>{this.props.title}</h1>
-        </header>
-        )
-    }
+class Header extends React.Component {
+	render() {
+		return (
+			<header className="header">
+				<div className="transitionBtn" onClick={this.props.onTransition}>
+					{this.props.title === "Lista de alunos" && <img src={AddImg} alt="asd" />}
+                    {this.props.title === "Cadastramento" && <img src={ArrowImg} alt="asd" id="arrow"/>}
+				</div>
+				<h1>{this.props.title}</h1>
+			</header>
+		);
+	}
 }
 
 export default Header;
