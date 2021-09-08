6f241afe00730ba06e9049d2894be325c2e0574b
Phelipe Alves Fagundes <81323315+howjack@users.noreply.github.com>
2021-09-08 12:06:22 -0300

    Changed Header to App.js

--- web-app/src/pages/Consulta/index.jsx
+++ web-app/src/pages/Consulta/index.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Header from "../../components/Header";
 import SearchBar from "../../components/SearchBar";
 import StudentList from "../../components/StudentsList";
 
@@ -8,7 +7,6 @@ class Consulta extends React.Component {
 	render() {
 		return (
 			<>
-				<Header title="Lista de alunos" onTransition={this.props.onTransition}/>
 				<SearchBar />
 				<StudentList onEdit={this.props.onEdit}/>
 			</>
--- web-app/src/pages/Registration/index.jsx
+++ web-app/src/pages/Registration/index.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Header from "../../components/Header";
 import RegistrationForms from "../../components/RegistrationForms";
 
 class Cadastro extends React.Component {
 	render() {
 		return (
 			<>
-				<Header title="Cadastramento" onTransition={this.props.onTransition} />
 				<RegistrationForms studentEdit={this.props.studentEdit} />
 			</>
 		);
