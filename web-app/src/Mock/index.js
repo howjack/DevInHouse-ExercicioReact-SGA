d59bc4e7eab2852db3932a126b179ab618d35e08
Phelipe Alves Fagundes <81323315+howjack@users.noreply.github.com>
2021-09-12 08:25:24 -0300

    ReCreate MockApi using Model

--- web-app/src/Mock/index.js
+++ web-app/src/Mock/index.js
@@ -1,88 +1,124 @@
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 console.log("MOCK");
 
-let students = [
-    {
-        showTextArea: true,
-        name: "Phelipe Alves Fagundes",
-        birthDate: "1998-06-26T03:00:00.000Z",
-        respName: "Valmor Fagundes",
-        respPhone: "5547991407271",
-        respWarningDegree: "Pai",
-        respWarningPhone: "5547991407271",
-        foodDescription: "Bolo, Chocolate",
-        photoAuthorization: true,
-        authorizedPersons: [
-            {
-                name: "João",
-                grau: "Tios"
-            },
-            {
-                name: "Marilda",
-                grau: "Tios"
-            }
-        ],
-        class: "202",
-        remarks: "Camarão"
-    },
-    {
-        showTextArea: true,
-        name: "Matheus Khorb",
-        birthDate: "1999-03-20T03:00:00.000Z",
-        respName: "Mulato Khorb",
-        respPhone: "5547991407271",
-        respWarningDegree: "Pai",
-        respWarningPhone: "5547991407271",
-        foodDescription: "Culher",
-        photoAuthorization: false,
-        authorizedPersons: [
-            {
-                name: "Alberto",
-                grau: "Tios"
-            },
-            {
-                name: "João",
-                grau: "Tios"
-            }
-        ],
-        class: "201",
-        remarks: "Pepino"
-    },
-    {
-        showTextArea: false,
-        name: "Cleverton Ruppenthal",
-        birthDate: "1999-04-16T03:00:00.000Z",
-        respName: "Rozeli Ruppenthal",
-        respPhone: "5547991407271",
-        respWarningDegree: "Mãe",
-        respWarningPhone: "5547991407271",
-        foodDescription: "",
-        photoAuthorization: true,
-        authorizedPersons: [
-            {
-                name: "Silverio",
-                grau: "Tios"
-            },
-            {
-                name: "Junior",
-                grau: "Padrinhos"
-            }
-        ],
-        class: "301",
-        remarks: "Nerd"
-    }
-]
-
 createServer({
+    models: {
+        student: Model,
+    },
+
+    seeds(server) {
+        server.create("student", {
+            id: 0,
+            showTextArea: true,
+            name: "Phelipe Alves Fagundes",
+            birthDate: "1998-06-26T03:00:00.000Z",
+            respName: "Valmor Fagundes",
+            respPhone: "5547999999999",
+            respWarningDegree: "Pai",
+            respWarningPhone: "5547999999999",
+            foodDescription: "Bolo, Chocolate",
+            photoAuthorization: true,
+            authorizedPersons: [
+                {
+                    name: "João",
+                    grau: "Tios"
+                },
+                {
+                    name: "Marilda",
+                    grau: "Tios"
+                }
+            ],
+            class: "202",
+            remarks: "Gordão"
+        })
+        server.create("student", {
+            id: 1,
+            showTextArea: true,
+            name: "Matheus Khorb",
+            birthDate: "1999-03-20T03:00:00.000Z",
+            respName: "Mulato Khorb",
+            respPhone: "5547888888888",
+            respWarningDegree: "Pai",
+            respWarningPhone: "5547888888888",
+            foodDescription: "Culher",
+            photoAuthorization: false,
+            authorizedPersons: [
+                {
+                    name: "Alberto",
+                    grau: "Tios"
+                },
+                {
+                    name: "João",
+                    grau: "Tios"
+                }
+            ],
+            class: "201",
+            remarks: "14,5"
+        })
+        server.create("student", {
+            id: 2,
+            showTextArea: false,
+            name: "Cleverton Ruppenthal",
+            birthDate: "1999-04-16T03:00:00.000Z",
+            respName: "Rozeli Ruppenthal",
+            respPhone: "5547777777777",
+            respWarningDegree: "Mãe",
+            respWarningPhone: "5547777777777",
+            foodDescription: "", //
+            photoAuthorization: true, //
+            authorizedPersons: [
+                {
+                    name: "Silverio",
+                    grau: "Tios"
+                },
+                {
+                    name: "Junior",
+                    grau: "Padrinhos"
+                }
+            ],
+            class: "301",
+            remarks: "Nerd"
+        })
+    },
+
     routes() {
         this.namespace = "api"
 
+        this.get("/students", (schema, request) => {
+            return schema.students.all()
+        })
+
+        this.get("/students/:id", (schema, request) => {
+            let id = request.params.id
+
+            return schema.students.find(id)
+        })
+
         this.post("/students", (schema, request) => {
             let attrs = JSON.parse(request.requestBody)
-            students.push(attrs)
-            console.log(students)
-            return {student: attrs}
+            let min = Math.ceil(3);
+            let max = Math.floor(1000);
+            attrs.id = Math.floor(Math.random() * (max - min + 1)) + min;
+            return schema.students.create(attrs)
         })
+
+        this.patch("/students/:id", (schema, request) => {
+            let newAttrs = JSON.parse(request.requestBody)
+            let id = request.params.id
+            let student = schema.students.find(id)
+
+            return student.update(newAttrs)
+        })
+
+        this.delete("/students/:id", (schema, request) => {
+            let id = request.params.id
+
+            return schema.students.find(id).destroy()
+        })
+
+
+
+
         this.get("/kindred", () => {
             return {
                 kindred: [
@@ -109,12 +145,6 @@ createServer({
                 ]
             }
         })
-        this.get("/students", () => {
-            return {
-                students
-            }
-        });
-
-    },
+    }
 
 })
