aee5f80ef423d4a84a27065293024d1d1007dffe
Phelipe Alves Fagundes <81323315+howjack@users.noreply.github.com>
2021-09-13 09:45:23 -0300

    Create EmployeeProvider.jsx

---
+++ web-app/src/Context/EmployeeProvider.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const EmployeeContext = React.createContext({});
+
+class EmployeeProvider extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			employee: null,
+			setEmployee: this.setEmployee,
+		};
+	}
+
+	setEmployee = (employee) => {
+		this.setState({ employee }, () => console.log(this.state.employee));
+	};
+	render() {
+		return (
+			<EmployeeContext.Provider value={this.state}>
+				{this.props.children}
+			</EmployeeContext.Provider>
+		);
+	}
+}
+
+export { EmployeeProvider, EmployeeContext };
