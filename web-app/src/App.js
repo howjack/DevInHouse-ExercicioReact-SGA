38c22ccb91129c410e3963d67db7d276646d2bec
Phelipe Alves Fagundes <81323315+howjack@users.noreply.github.com>
2021-09-13 16:46:52 -0300

    I added if-else to decide if it's connected

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -5,20 +5,49 @@ import { Switch, Route, BrowserRouter } from "react-router-dom";
 import Consulta from './pages/Consulta'
 import Cadastro from './pages/Registration'
 import Detail from './pages/Detail';
-class App extends React.Component {
+import { APIProvider } from './Context/ApiProvider';
+import { StudentProvider } from './Context/StudentProvider';
+import Login from './pages/Login';
+import EmployeeDetail from './pages/EmployeeDetail';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isLogin: true,
+      setLogin: this.setLogin
+    }
+  }
+
+  setLogin = (isLogin) => {
+    this.setState({ isLogin })
+  }
   render() {
-    return (
+    if (this.state.isLogin) {
+
+      return (
+        <Login setLogin={this.setLogin} />
+      )
+    } else {
+      return (
         <BrowserRouter>
-          <Header />
-          <Switch>
-            <Route path="/" exact />
-            <Route path="/consulta" component={Consulta} />
-            <Route path="/cadastro" component={Cadastro} />
-            <Route path="/detalhe/:id" component={Detail} />
-          </Switch>
+          <APIProvider>
+            <StudentProvider>
+              <Header />
+              <Switch>
+                <Route path="/" exact />
+                <Route path="/consulta" component={Consulta} />
+                <Route path="/cadastro" component={Cadastro} />
+                <Route path="/detalhe/:id" component={Detail} />
+                <Route path="/employee" component={EmployeeDetail} />
+              </Switch>
+            </StudentProvider>
+          </APIProvider>
         </BrowserRouter>
-    );
+      );
+    }
+
   };
 }
 
